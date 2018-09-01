7dfa5301d3e9b56ea4955be61e5976fe87c17145
pkellner <peter@peterkellner.net>
2018-08-31 18:55:25 -0700

    added m5-04

---
+++ Progressions/m5-rest-calls/04-More-Redux/reactapp/ClientApp/Components/speakers/Speakers.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import SpeakersHeader from './SpeakersHeader';
+import SpeakerList from './SpeakerList';
+import axios from 'axios';
+
+class Speakers extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            appData: []
+        };
+    }
+
+
+
+    componentDidMount() {
+        axios.get('/data/speakers.json')
+            .then((result)=> {
+                this.setState({
+                    appData: result.data,
+                    isLoading: false
+                });
+            })
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.responderEnd);
+                }
+            });
+    }
+
+
+    render() {
+
+        if (this.state.isLoading) {
+            return <span><i>Loading...</i></span>
+        }
+        else {
+            return (
+                <div>
+                    <SpeakersHeader/>
+                    <SpeakerList speakers={this.state.appData} />
+                    {/*<span>{JSON.stringify(this.state.appData)}</span>*/}
+                </div>
+            );
+        }
+    }
+}
+
+Speakers.propTypes = {};
+Speakers.defaultProps = {};
+
+export default Speakers;
+
+
+// import React from 'react';
+// import SpeakersHeader from './SpeakersHeader';
+//
+// export default function Speakers(props) {
+//
+//
+//
+//
+//     return (
+//         <div>
+//             <SpeakersHeader/>
+//         </div>
+//     );
+// }
