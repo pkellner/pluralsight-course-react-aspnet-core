d464f370544fa84bf81f0b5cf48b240208692b4d
pkellner <peter@peterkellner.net>
2019-01-04 15:38:03 -0800

    Removed package-lock.json and fixed RouteNotFound to include proper handling of routeStatus as was always the case in module 5 and beyond. Module 2 was the only problem.

--- Progressions/m2-custom-webpack/08-Splitting-the-WebPack-Config/reactapp/ClientApp/RouteNotFound.js
+++ Progressions/m2-custom-webpack/08-Splitting-the-WebPack-Config/reactapp/ClientApp/RouteNotFound.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
 
+
+
 class RouteNotFound extends Component {
+
+    componentDidMount() {
+        this.props.action(true);
+    }
+
     render() {
+        console.log('route not found in RouteNotFound.js.  Bubble up...');
+        //this.props.action(false);
         return (
-            <Route render={() => {
+            <Route render={({ staticContext }) => {
+                if (staticContext) {
+                    staticContext.status = 404;
+                }
                 return (
                     <div>
                         <h1>404 : Not Found!</h1>
@@ -18,3 +30,34 @@ class RouteNotFound extends Component {
 RouteNotFound.defaultProps = {};
 
 export default RouteNotFound;
+
+// 1/4/2019: NOTE
+// I'M LEAVING THE BELOW CODE IN COMMENTED OUT JUST TO MAKE IT EXTRA CLEAR THAT THIS WAS THE CODE
+// BEFORE THE 1/4/2019 UPDATED AND THE NEW CODE ABOVE REPLACES IT.
+// THANKS GO TO @PATRICKWEEGAN FOR REPORTING THE ISSUE
+// https://github.com/pkellner/pluralsight-course-react-aspnet-core/issues/3
+// I DON'T LIKE LEAVING IN COMMENTED CODE, BUT IN THIS CASE, I THINK IT IS HELPFUL TO AVOID CONFUSION IF SOMEONE HAS
+// OLD CODE AROUND AND DOES NOT REALIZE IT WAS UPDATED. THIS MAKES IT EXTRA CLEAR -PETER KELLNER
+//
+
+// import React, {Component} from 'react';
+// import { Route } from 'react-router-dom';
+//
+// class RouteNotFound extends Component {
+//     render() {
+//         return (
+//             <Route render={() => {
+//                 return (
+//                     <div>
+//                         <h1>404 : Not Found!</h1>
+//                     </div>
+//                 )
+//             }}/>
+//         );
+//     }
+// }
+//
+// RouteNotFound.defaultProps = {};
+//
+// export default RouteNotFound;
+
